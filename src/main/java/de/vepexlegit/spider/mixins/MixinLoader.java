df55595e81bb6e82e25c62a881be4bdd243f5c1a
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-08-29 00:36:50 +0400

    Create MixinLoader.java

---
+++ src/main/java/de/vepexlegit/spider/mixins/MixinLoader.java
@@ -0,0 +1,38 @@
+package de.vepexlegit.spider.mixins;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+import java.util.Map;
+
+public class MixinLoader implements IFMLLoadingPlugin {
+    public MixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.spider.json");
+        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {}
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}

32fc60b791152e80376ccb52db5a7f8736ed4c92
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-08-29 00:38:46 +0400

    Create MixinEntity.java

---
+++ src/main/java/de/vepexlegit/spider/mixins/MixinEntity.java
@@ -0,0 +1,21 @@
+package de.vepexlegit.spider.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+
+    private static Minecraft mc = Minecraft.getMinecraft();
+
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onUpdate(final CallbackInfo ci) {
+        if (mc.thePlayer.isCollidedHorizontally) {
+            mc.thePlayer.motionY = 0.25;
+        }
+    }
+}
